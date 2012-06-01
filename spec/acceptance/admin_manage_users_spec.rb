0ae7b45ae0b31a4b722117c37126240ad9ae6d3c
Luciano Tavares <lucianomt@gmail.com>
2012-06-01 15:23:11 -0300

    added admin integration test

--- spec/acceptance/admin_manage_users_spec.rb
+++ spec/acceptance/admin_manage_users_spec.rb
@@ -2,20 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
 feature 'admin manage users' do
   
-  # context 'login' do
-  #   scenario 'valid' do
-  #     admin = User.make!(:admin)
-  #     login(admin)
-  #     admin.has_role?(:admin).should be_true
-  #     page.should have_content 'Admin'
-  #   end
+  context 'login' do
+    scenario 'valid' do
+      admin = User.make!
+      admin.add_role :admin
+      login(admin)
+      admin.has_role?(:admin).should be_true
+      page.should have_link 'Admin'
+    end
 
-  #   scenario 'invalid' do
-  #     user = User.make!(:email => 'user@example.com')
-  #     login(user)
-  #     user.has_role?(:admin).should be_false
-  #     page.should_not have_content 'Admin'
-  #   end
-  # end
+    scenario 'invalid' do
+      user = User.make!
+      login(user)
+      user.has_role?(:admin).should be_false
+      page.should_not have_link 'Admin'
+    end
+  end
 
 end
