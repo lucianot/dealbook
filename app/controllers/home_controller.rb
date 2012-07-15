d5118fe1cf7a2f21086f749b057dbec375611628
Luciano Tavares <lucianomt@gmail.com>
2012-07-14 23:05:41 -0300

    added homepage for guests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @deals = Deal.order('updated_at DESC').limit(10)
+    @deal_count = Deal.count
+    @investor_count = Investor.count
+    @company_count = Company.count
   end
 end
