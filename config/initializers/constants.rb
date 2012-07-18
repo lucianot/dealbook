2f62faab160c730d74868cc48fe2ea60fb25356d
Luciano Tavares <lucianomt@gmail.com>
2012-07-17 22:11:37 -0300

    fixed linkedin regex

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
   # Regex
   URL_REGEX = %r{^(http|https)://[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?/.*)?$}ix
-  LINKEDIN_COMPANY_REGEX = %r{^(http|https)://[a-z0-9]+\.linkedin\.com/company/[0-9]+.*$}ix
-  LINKEDIN_REGEX = %r{^(http|https)://[a-z0-9]+\.linkedin\.com/(profile/view\?id=|company/)?[0-9]+.*$}ix
+  LINKEDIN_COMPANY_REGEX = %r{^(http|https)://[a-z0-9]+\.linkedin\.com/company/.*$}ix
+  LINKEDIN_REGEX = %r{^(http|https)://[a-z0-9]+\.linkedin\.com/(profile/view\?id=|company/)?.*$}ix
