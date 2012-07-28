ba50bf7dd856b75408851062401ff3a79066f855
Luciano Tavares <lucianomt@gmail.com>
2012-07-28 00:43:15 -0300

    added create deal link to company page

--- spec/acceptance/display_companies_spec.rb
+++ spec/acceptance/display_companies_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
 feature 'display companies' do
   
-  context 'unauthenticated user' do
+  context 'guest' do
 
     scenario 'no companies' do
       visit '/'
@@ -29,9 +29,36 @@ feature 'display companies' do
       page.should have_content company.linkedin
       page.should have_content company.status          
       #TODO: include markets, locations
+      page.should_not have_link 'Add new deal'
       page.should_not have_link 'Edit'
+      page.should_not have_link 'Destroy'      
       page.should have_link 'Back'
     end
+  end
 
+  context 'user' do
+    scenario 'show company' do
+      company = Company.make!
+      login_normal
+      click_link 'Companies'
+      click_link company.name
+      page.should have_content company.name
+      page.should have_content company.description
+      page.should have_content company.website
+      page.should have_content company.linkedin
+      page.should have_content company.status          
+      #TODO: include markets, locations
+      page.should have_link 'Add new deal'
+      page.should have_link 'Edit'
+      page.should have_link 'Destroy'
+      page.should have_link 'Back'
+      click_link 'Add new deal'
+      page.should have_content 'Create new deal'
+      uri = URI.parse(current_url)
+      uri.path.should == "/deals/new"     
+    end
   end
 end
+
+
+
