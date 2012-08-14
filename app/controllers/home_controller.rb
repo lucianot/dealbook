4654e6b6c1e5cabf0eb2fd194e139d92b2851665
dttg <diego.ttg@gmail.com>
2012-08-13 22:24:45 -0300

    new index ready for feedback

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @deal_count = Deal.count
     @investor_count = Investor.count
     @company_count = Company.count
+    @user_count = User.count
 
     if user_signed_in?
       flash[:mixpanel_event] = "name_tag('#{current_user.full_name} : #{current_user.email}')"
