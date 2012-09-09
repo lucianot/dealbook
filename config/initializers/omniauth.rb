08c2adf278da7399da5a773ae9347b370e75f30c
dttg <diego.ttg@gmail.com>
2012-09-09 15:04:03 -0300

    removing old omniauth initializer

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin, '4h7odlmlcsi3', 'HCmhbtXLBchaJIez'
-  provider :twitter, 'Z61i6268bRaxgJlWHyDnw', 'kQDI1aerhQaqqZyaxcFMUXoRbeN4RqAeWCjmLwRA'
-end
