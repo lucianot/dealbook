bd8cb2a018710b8d108b63048b56b0f33c063ae9
Edmar Ferreira <edmaroliveiraferreira@gmail.com>
2012-09-23 19:49:08 -0300

    add valid scenario

--- spec/acceptance/authenticate/signup_with_linkedin_spec.rb
+++ spec/acceptance/authenticate/signup_with_linkedin_spec.rb
@@ -1,30 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/../acceptance_helper')
 
 feature 'signup with linkedin' do
-  # LINKEDIN_INFO = {
-  #   "email-address" => 'user@example.com'
-  # }
-
-  before do 
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:linkedin] = OmniAuth::AuthHash.new({
-      :provider => 'linkedin',
-      :uid => '12345'
-    })
-  end
-  
-  after { OmniAuth.config.test_mode = false }
 
   scenario 'valid' do
+		user = User.make
     visit "/"
     click_link 'Join Now'
     click_link 'Sign Up with Linkedin'
-    response.should_not == nil
-    json = ActiveSupport::JSON.decode(response.body)
-
-    json["uid"].should == '12345'
-    json["provider"].should == 'linkedin'
-    # json["info"]["email-address"] == 'user@example.com'
+  	fill_in 'Full name', with: user.full_name
+  	fill_in 'Email', with: user.email
+  	fill_in 'Password', with: user.password
+  	fill_in 'Password confirmation', with: user.password
+  	click_button 'Sign up'
+		page.should have_content "Welcome! You have signed up successfully"
   end
 
   scenario 'invalid'
