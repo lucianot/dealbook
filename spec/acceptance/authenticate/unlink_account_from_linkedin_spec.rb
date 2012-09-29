1c13efb83fc545b52c927612c567af1d4a2d3cc5
dttg <diego.ttg@gmail.com>
2012-09-29 18:57:42 -0300

    drafting unlink tests

--- spec/acceptance/authenticate/unlink_account_from_linkedin_spec.rb
+++ spec/acceptance/authenticate/unlink_account_from_linkedin_spec.rb
@@ -1,15 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/../acceptance_helper')
 
 feature 'unlink account from linkedin' do
+  before do 
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:linkedin] = OmniAuth::AuthHash.new({
+      :provider => 'linkedin',
+      :uid => '12345',
+      :info => { :name => 'user', :email => "user@example.com" }
+      })
+  end
+  
+  after { OmniAuth.config.test_mode = false }
+
+  scenario 'valid' do
+    visit "/"
+    click_link 'Join Now'
+    click_link 'Sign Up with Linkedin'
+    visit "/users/edit"
+    click_link 'Unlink Linkedin account'
+    page.should have_content ""
+  end
+  
   context 'user has password' do
     scenario 'unlink successful'
-    #   login_normal
-    #   visit "/users/edit"
-    #   click_link 'Disconnect Linkedin account'
-    #   page.should have_content "Success! Your Linkedin account is linked."
-    #   page.should have_content "Link to your Linkedin account"
-    # end
-
     scenario 'unlink fail'
   end
 
