786eb954ebc159f793f35e91aa2998f6e9f76e8b
Luciano Tavares <lucianomt@gmail.com>
2012-10-14 19:00:49 -0300

    added initializer for mailer constants for devise

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load heroku vars from local file
+heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
+load(heroku_env) if File.exists?(heroku_env)
+
 # Initialize the rails application
 Dealbook::Application.initialize!
