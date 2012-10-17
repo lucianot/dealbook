a034e8753fe18ffb5cc6aa5fc0b00248457ead4e
dttg <diego.ttg@gmail.com>
2012-10-16 21:49:22 -0300

    passing valid scenario for linkedin account linking

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
-      flash.notice = "Signed in!"
+      flash.notice = "Sucessfully logged in with Linkedin!"
       sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
--- spec/acceptance/authenticate/link_account_to_linkedin_spec.rb
+++ spec/acceptance/authenticate/link_account_to_linkedin_spec.rb
@@ -1,14 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../acceptance_helper')
 
 feature 'link account to linkedin' do
-  
+  before do 
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:linkedin] = OmniAuth::AuthHash.new({
+      :provider => 'linkedin',
+      :uid => '12345',
+      :info => { :name => 'user', :email => "user@example.com" }
+    })
+  end
   scenario 'valid linkedin credentials'
-  #   login_normal
-  #   visit "/users/edit"
-  #   click_link 'Link to your Linkedin account'
+  scenario 'valid' do
+    user = User.make!
+    login(user)
+    visit "/users/edit"
+    click_link 'Associate a Linkedin account'
+    page.should have_content "Sucessfully logged in with Linkedin!"
+    
+  end
+  #   
   #   # Insert credentials?
   #   page.should have_content "Success! Your Linkedin account is linked."
-  #   page.should have_content "Unlink Linkedin account"
+  #   
   # end 
 
   scenario 'invalid linkedin credentials'
