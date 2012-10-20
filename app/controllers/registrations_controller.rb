3ea389a502f170521ed6a98cf3b7e8b0df5ee8ca
dttg <diego.ttg@gmail.com>
2012-10-19 22:31:31 -0300

    unlinking users

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+  def update
+    if resource.update_with_password(params[resource_name])
+      set_flash_message :notice, :updated
+      sign_in resource_name, resource, :bypass => true
+      redirect_to after_update_path_for(resource)
+    else
+      clean_up_passwords(resource)
+      render_with_scope :edit
+    end
+  end
+end
