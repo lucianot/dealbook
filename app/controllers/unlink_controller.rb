fc8fba2c4c3a5c4db82db479a669985edb2a041d
Luciano Tavares <lucianomt@gmail.com>
2012-11-18 22:18:57 -0200

    changed edit route to put for consistency

--- app/controllers/unlink_controller.rb
+++ app/controllers/unlink_controller.rb
@@ -1,5 +1,5 @@
 class UnlinkController < ApplicationController
-  def create
+  def new
     @user = current_user 
     @user.unlink_from_linkedin
     redirect_to "/users/edit", notice: "Your Linkedin account has been unlinked from your profile."
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Dealbook::Application.routes.draw do
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout" },
                     controllers: { omniauth_callbacks: "omniauth_callbacks" }
                     
-  get "/users/unlink" => "unlink#create", as: "unlink"
+  put "/users/unlink" => "unlink#new", as: "unlink"
 
   resources :users, :only => [:show, :index, :edit, :update]
   resources :deals do
