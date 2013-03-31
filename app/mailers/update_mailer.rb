2c24dc8c3a933ccab258b4846c25e447e51b7531
Luciano Tavares <lucianomt@gmail.com>
2013-03-31 20:37:27 -0300

    fixed url not showing

--- app/mailers/update_mailer.rb
+++ app/mailers/update_mailer.rb
@@ -10,10 +10,12 @@ class UpdateMailer < ActionMailer::Base
     @subject = subject
     @header = header
     @object_title = title
-    @object_url = action != 'destroy'? url_for(@object) : ''
+    @object_url = url
 
     mail(:to => 'luciano@tavares.us', :subject => @subject)
   end
+
+  private
 
   def subject
     case @action
@@ -29,7 +31,7 @@ class UpdateMailer < ActionMailer::Base
   def header
     case @action
     when 'create'
-      "#A new #{@model.downcase} has just been added to Dealbook:"
+      "A new #{@model.downcase} has just been added to Dealbook:"
     when 'update'
       "#{preposition} #{@model.downcase} has just been updated on Dealbook:"
     when 'destroy'
@@ -39,6 +41,21 @@ class UpdateMailer < ActionMailer::Base
 
   def title
     @model == 'Deal' ? @object.headline : @object.name
+  end
+
+  def url
+    if @action == 'destroy'
+      ''
+    else
+      case @model
+      when 'Company'
+        company_path(@object)
+      when 'Investor'
+        investor_path(@object)
+      when 'Deal'
+        deal_path(@object)
+      end
+    end
   end
 
   def preposition
